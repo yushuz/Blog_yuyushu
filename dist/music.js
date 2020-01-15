2edc832bac77f9095d3bb660d72c59d75928e8de
Yushu Zhang <yushuz@umich.edu>
2020-01-14 20:02:18 -0500

    Site updated: 2020-01-14 20:02:18

---
+++ dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "我只在乎你",
+        artist: '王若琳',
+        url: 'https://link.hhtjim.com/qq/004E36yw08XcCB.mp3',
+        cover: 'https://cdn.smehost.net/sonymusiccomcn-auprod/wp-content/uploads/2019/12/w2.png'
+      },
+	  {
+        name: "See You Again",
+        artist: '窦靖童',
+        url: 'http://www.ytmp3.cn/down/43864.mp3',
+        cover: 'http://n.sinaimg.cn/ent/transform/20170906/15Cl-fykpyuf9312853.jpg'
+      },
+	  {
+        name: "Flying Pickets - Only You",
+        artist: '陈勋奇',
+        url: 'https://link.hhtjim.com/163/68284.mp3',
+        cover: 'http://p1.music.126.net/t8jj-QUxn3QZstvECbb64g==/42880953495299.jpg'
+      },
+	  {
+        name: "男孩别哭",
+        artist: '海龟先生',
+        url: 'http://www.ytmp3.cn/down/39116.mp3',
+        cover: 'https://i.kfs.io/album/global/4567513,0v1/fit/500x500.jpg'
+      },
+	  {
+        name: "西湖",
+        artist: '痛仰乐队',
+        url: 'https://link.hhtjim.com/163/381849.mp3',
+        cover: 'https://i.kfs.io/album/global/4567513,0v1/fit/500x500.jpg'
+      },
+	  {
+        name: "太聪明",
+        artist: '陈绮贞',
+        url: 'https://link.hhtjim.com/163/209397.mp3',
+	cover: ''
+      },
+	  {
+        name: "小鱼",
+        artist: '魏如萱',
+        url: 'https://link.hhtjim.com/163/442867306.mp3',
+        cover: ''
+      },
+    ]
+});
